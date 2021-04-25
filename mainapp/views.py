655696b319c6bcfb49453078aa5c6babec03b98b
Люция <lvaleeva87@gmail.com>
2021-04-25 21:14:25 +0500

    added correct work with static

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'GeekShop',
+        'header': 'добро пожаловать на сайт',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'GeekShop - каталог',
+        'header': 'GeekShop',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 6090.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590.00},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890.00},
+        ]
+    }
+    return render(request, 'mainapp/products.html', context)
