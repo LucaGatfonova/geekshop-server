ee10ce21c539767f23fa3ed26ee5eb0d51b4dfdd
Люция <lvaleeva87@gmail.com>
2021-05-15 21:11:44 +0500

    added login_required and logout

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from authapp.views import login, register, profile
+from authapp.views import login, register, profile, logout
 
 app_name = 'authapp'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
 ]
