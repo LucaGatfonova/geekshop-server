5d0a279f88a191a5908e28db71e943278df339c4
Люция <lvaleeva87@gmail.com>
2021-05-23 14:49:18 +0500

    added create users

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
 
 
 def index(request):
     return render(request, 'adminapp/admin.html')
 
+
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
