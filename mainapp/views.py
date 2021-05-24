d0908059057c6fe53aecfe75b1b1f65769071b03
Люция <lvaleeva87@gmail.com>
2021-05-24 19:16:44 +0500

    added filtering by category_id

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,11 +13,11 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'GeekShop - Каталог',
-        'header': 'GeekShop',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+def products(request, category_id=None):
+    context = {'title': 'GeekShop - Каталог', 'header': 'GeekShop', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+        context.update({'products': products})
+    else:
+        context.update({'products': Product.objects.all()})
     return render(request, 'mainapp/products.html', context)
